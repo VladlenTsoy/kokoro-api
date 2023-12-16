b5d0a5439bae048992b8233cbe4c6036025019fc
vladlen <vladlengg@gmail.com>
2023-12-16 20:06:52 +0500

    remove unused imports

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,8 @@
 import {Injectable} from "@nestjs/common"
-import {ProductCategoryEntity} from "./modules/product-category/entities/product-category.entity"
-import {InjectRepository} from "@nestjs/typeorm"
-import {Repository} from "typeorm"
 
 @Injectable()
 export class AppService {
-    constructor(
-        @InjectRepository(ProductCategoryEntity)
-        private categoryRepository: Repository<ProductCategoryEntity>
-    ) {
-    }
-
     async getHello(): Promise<string> {
-        const category = this.categoryRepository.create({title: "Верхняя одежда"})
-        await this.categoryRepository.save(category)
-        console.log(category)
-        return category.url
+        return "Hello"
     }
 }
