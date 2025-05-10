99174f37df256ed417af998c24b14ef8cfa92e9e
vladlentsoy <vladlengg@gmail.com>
2025-05-10 13:58:03 +0500

    fix eslint

--- src/modules/admin/product-property/dto/update-product-property.dto.ts
+++ src/modules/admin/product-property/dto/update-product-property.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductPropertyDto } from './create-product-property.dto';
+import {PartialType} from "@nestjs/swagger"
+import {CreateProductPropertyDto} from "./create-product-property.dto"
 
 export class UpdateProductPropertyDto extends PartialType(CreateProductPropertyDto) {}
