12345a7bcd55b7920c2b348b500db43b0bd5e192
vladlentsoy <vladlengg@gmail.com>
2025-09-17 04:46:39 +0500

    remove unused import

--- src/modules/admin/country/entities/country.entity.ts
+++ src/modules/admin/country/entities/country.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn} from "typeorm"
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm"
 import {ApiProperty} from "@nestjs/swagger"
 
 @Entity("countries")
