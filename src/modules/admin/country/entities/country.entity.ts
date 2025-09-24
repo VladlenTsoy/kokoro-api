91071b32eb24d411ae979ee872d15e5c12d9b410
vladlentsoy <vladlengg@gmail.com>
2025-09-24 15:40:06 +0500

    delete unused imports

--- src/modules/admin/country/entities/country.entity.ts
+++ src/modules/admin/country/entities/country.entity.ts
@@ -1,4 +1,4 @@
-import {Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm"
+import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm"
 import {ApiProperty} from "@nestjs/swagger"
 import {CityEntity} from "../../city/entities/city.entity"
 
